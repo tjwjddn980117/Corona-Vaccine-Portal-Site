9ef028fb90caa076bd7a129b324872fd4b152beb
SeungMin_Hyun <blackcoding911@gmail.com>
2021-07-19 21:44:19 +0900

    loginPage

--- prototype/src/pages/LoginPage.vue
+++ prototype/src/pages/LoginPage.vue
@@ -1,16 +1,45 @@
 <template>
-  <div>
-    abc
-  </div>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <font-awesome-icon icon="faaddresscard" />
+      <lable for = "username">id: </lable>
+      <input id="username" type="text" v-model="username">
+    </div>
+    <div>
+      <lable for = "password">pw: </lable>
+      <input id="password" type="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
 </template>
 <script>
+  import axios from 'axios';
   export default {
   components: { },
     name: 'LoginPage',
     data() {
-      return {}
+      return {
+        username: '',
+        password: '',
+      }
     },
-    methods: {},
+    methods: {
+      submitForm: function(){
+        console.log(this.id, this.password);
+        var url = 'https://jsonplaceholder.typicode.com/users';
+        var data = {
+          username: this.username,
+          password: this.password
+        }
+        axios.post(url, data)
+        .then(function(response){
+          console.log(response);
+        })
+        .catch(function(error){
+          console.log(error);
+        })
+      }
+    },
     created() {}
   }
 </script>
